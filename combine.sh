a01193b31a57cad6b47b357142be721b373039da
JustSong <quanpengsong@gmail.com>
2023-02-26 23:01:49 +0800

    fix: update fps

--- combine.sh
+++ combine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python process.py --input $1 --output $1_processed
-ffmpeg -r 30 -i $1_processed/%04d.jpg -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p $1.mp4
+ffmpeg -r 10 -i $1_processed/%04d.jpg -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p $1.mp4
