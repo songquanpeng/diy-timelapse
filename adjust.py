0616f77fd24a49a42bd5646361cc988a223712f4
JustSong <songquanpeng@foxmail.com>
2023-03-02 16:46:15 +0800

    feat: able to adjust tint & temp

--- adjust.py
+++ adjust.py
@@ -7,8 +7,10 @@ import os
 parser = argparse.ArgumentParser(description='给一系列图片添加滤镜效果')
 parser.add_argument('--input', type=str, default="test", help='需要处理的图片所在目录')
 parser.add_argument('--output', type=str, default="test_processed", help='处理后的图片要保存的目录')
-parser.add_argument('--saturation_factor', type=float,
+parser.add_argument('--saturation', type=float,
                     default=1.5, help='饱和度增加的因子')
+parser.add_argument('--temperature', type=float, default=0, help='色温调整的值，取值范围为-100到100，默认为0')
+parser.add_argument('--tint', type=float, default=0, help='色调调整的值，取值范围为-100到100，默认为0')
 
 # 解析命令行参数
 args = parser.parse_args()
@@ -25,18 +27,22 @@ for filename in tqdm(filenames):
             # 将图片的饱和度增加
             img = img.convert('HSV')
             h, s, v = img.split()
-            s = s.point(lambda x: x * args.saturation_factor)
+            s = s.point(lambda x: x * args.saturation)
             img = Image.merge('HSV', (h, s, v)).convert('RGB')
-            # 将图片的天空颜色更改为蓝色
-            # pixels = img.load()
-            # for i in range(img.size[0]):
-            #     for j in range(img.size[1]):
-            #         r, g, b = pixels[i, j]
-            #         if b > r and b > g:
-            #             pixels[i, j] = (r, g, int(b * 1.2))
-            # 将处理后的图片保存到输出路径
-            # output_filename = os.path.splitext(filename)[0] + '_filtered.jpg'
+            # 调整图片的色温
+            if args.temperature != 0:
+                if args.temperature > 0:
+                    r, g, b = 255, 255 - args.temperature, 255 - args.temperature
+                else:
+                    r, g, b = 255 + args.temperature, 255 + args.temperature, 255
+                color_matrix = (r / 255, 0, 0, 0, 0, g / 255, 0, 0, 0, 0, b / 255, 0)
+                img = img.convert('RGB', color_matrix)
+            # 调整图片的色调
+            if args.tint != 0:
+                r, g, b = img.split()
+                r = r.point(lambda x: x + args.tint)
+                img = Image.merge('RGB', (r, g, b))
             output_filename = filename
             img.save(os.path.join(args.output, output_filename))
 
-print('饱和度调整处理完成！')
+print('图像处理完成！')
--- generate_video.sh
+++ generate_video.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python process.py --input $1 --output $1_processed
-python adjust_saturation.py --input $1_processed --output $1_processed --saturation_factor 1.8
+python adjust.py --input $1_processed --output $1_processed --saturation 2 --temperature 0 --tint 0
 ffmpeg -r 10 -i $1_processed/%04d.jpg -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p $1.mp4 -y
